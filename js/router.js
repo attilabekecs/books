cccf28322d8484efe6d3dc6c1dcec88c924246c7
attilabekecs <attila.bekecs@icloud.com>
2026-02-22 07:36:44 +0100

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,22 +1,47 @@
-import state from "./state.js";
 import { renderHome } from "./views/homeView.js";
 import { renderLibrary } from "./views/libraryView.js";
 import { renderFavorites } from "./views/favoritesView.js";
 import { renderStats } from "./views/statsView.js";
+import { renderDetail } from "./views/detailView.js";
+import { renderEdit } from "./views/editView.js";
 
-export function render(root) {
+export function renderApp(state, root){
 
-  switch (state.view) {
+  if (state.loading){
+    root.innerHTML = `<div class="loading">Betöltés...</div>`;
+    return;
+  }
+
+  if (state.error){
+    root.innerHTML = `<div class="loading">Hiba: ${state.error}</div>`;
+    return;
+  }
+
+  switch (state.view){
+
     case "library":
       root.innerHTML = renderLibrary(state);
       break;
+
     case "favorites":
       root.innerHTML = renderFavorites(state);
       break;
+
     case "stats":
       root.innerHTML = renderStats(state);
       break;
+
+    case "detail":
+      root.innerHTML = renderDetail(state);
+      break;
+
+    case "edit":
+      root.innerHTML = renderEdit(state);
+      break;
+
+    case "home":
     default:
       root.innerHTML = renderHome(state);
+      break;
   }
 }
