09970c460508cfc74372ce336357bfacc65e9cf5
attilabekecs <attila.bekecs@icloud.com>
2026-02-22 07:28:33 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
-export const API_URL = "https://YOUR_GOOGLE_SCRIPT_URL";
-export const CACHE_KEY = "books_cache_v1";
-export const FAVORITES_KEY = "books_favorites";
-export const PROGRESS_KEY = "books_progress";
-export const THEME_KEY = "books_theme";
+export const API_URL = "https://script.google.com/macros/s/AKfycbzhgxR3zWWv20xdMG6c_FPMs5XSoNOuW5UvmQQ4bLbdkoZQmraD2V3ldFsJyn3MOAbXKQ/exec";
+
 export const VERSION_URL = "version.json";
+
+export const CACHE_KEY = "bookplex_cache_v2";
+export const PROGRESS_KEY = "bookplex_progress_v1";
+export const THEME_KEY = "bookplex_theme_v1";
