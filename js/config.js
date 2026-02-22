cd574e9d19065df5fff80e465591220e8a2bab86
attilabekecs <attila.bekecs@icloud.com>
2026-02-22 07:13:35 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = "https://YOUR_GOOGLE_SCRIPT_URL";
+export const CACHE_KEY = "books_cache_v1";
+export const FAVORITES_KEY = "books_favorites";
+export const PROGRESS_KEY = "books_progress";
+export const THEME_KEY = "books_theme";
+export const VERSION_URL = "version.json";
